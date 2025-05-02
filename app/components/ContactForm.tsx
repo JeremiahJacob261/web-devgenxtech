20ac7ccdafd1576912d315aca45f61fa4ae9facf
Jerry <jeremiahjacob261@gmail.com>
2025-05-02 22:56:10 +0100

    fix

--- app/components/ContactForm.tsx
+++ app/components/ContactForm.tsx
@@ -7,7 +7,7 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import * as z from "zod"
 import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
-import { supabase } from '@/lib/supabase'
+import supabase from '@/lib/supabase'
 import { Button } from "@/components/ui/button"
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 
